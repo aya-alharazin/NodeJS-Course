cc58e1974951a1aad5b0feff4d6258f2bceccb39
Aya <alharazinaya@gmail.com>
2026-04-06 18:37:54 +0200

    Add getBooks function in bookController.js to retrieve all books from the database and return as JSON response.

---
+++ 08 - all/controllers/bookController.js
@@ -0,0 +1,9 @@
+const {dbConnection} = require('../config')
+const getBooks = (req,res,next)=>{
+    dbConnection('books',async (collection)=>{
+        const books = await collection.find({}).toArray()
+        res.json(books)
+    })
+}
+
+module.exports = getBooks
