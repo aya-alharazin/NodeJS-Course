011fbfcb2e3a26589776486a6a9b293b53f5e990
Aya <alharazinaya@gmail.com>
2026-04-06 22:19:35 +0200

    Add pagination support to getBooks and implement getBooksPageCount for total page calculation in bookController. Update routes to include new endpoint for page count.

--- 08 - all/controllers/bookController.js
+++ 08 - all/controllers/bookController.js
@@ -1,9 +1,29 @@
 const {dbConnection} = require('../config')
 const getBooks = (req,res,next)=>{
+    const pageNumber = parseInt(req.query.page)
+    if(isNaN(pageNumber)){
+        res.status(400).json({
+            status:false,
+            message:"page not found"
+        })
+    }
+    const limit = 1
+    const skip = (pageNumber-1)*limit
      dbConnection('books',async (collection)=>{
-        const books = await collection.find({}).toArray()
+        const books = await collection.find({}).limit(limit).skip(skip).toArray()
         res.send(books)
     })
 }
 
-module.exports = {getBooks}
+const getBooksPageCount = (req,res,next)=>{
+    dbConnection('books',async (collection)=>{
+        const limit =1
+        const count = await collection.count({})
+        const noOfPages = Math.ceil(count/limit)
+        res.status(200).json({
+            "page Counts":noOfPages
+        })
+    })
+}
+
+module.exports = {getBooks,getBooksPageCount}
--- 08 - all/routes/bookRoutes.js
+++ 08 - all/routes/bookRoutes.js
@@ -2,6 +2,8 @@ const {Router} = require('express')
 const {bookController} = require('../controllers')
 const router = Router()
 router.get('/',bookController.getBooks)
+router.get('/books-count',bookController.getBooksPageCount)
+
 
 
 module.exports = router 
