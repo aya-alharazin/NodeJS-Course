37f58cbd7800a9eda82b72b2fb01d5f7b63f2bb6
Aya <alharazinaya@gmail.com>
2026-04-07 09:30:02 +0200

    Add save method to User class for database insertion and create a sample user instance. This enhances user data management by enabling persistence in the database.

--- 08 - all/models/User.js
+++ 08 - all/models/User.js
@@ -1,5 +1,21 @@
+const {dbConnection} = require('../config')
 class User{
     constructor(userData){
         this.userData=userData
     }
+
+    save(){
+        dbConnection('users',async (collection)=>{
+            await collection.insertOne(this.userData)
+        })
+    }
 }
+
+
+const user = new User({
+    name :"aya",
+    email:"alharazinaya@gmail.com",
+    username:"ahh1",
+    password:12345
+})
+user.save()
