f6820cf1211059f914e392eab77f6b23ee0a017e
Aya <alharazinaya@gmail.com>
2026-04-07 10:07:54 +0200

    Add validation method to User class using UserValidator for improved data integrity. Update user instance creation with new validation logic and sample data.

--- 08 - all/models/User.js
+++ 08 - all/models/User.js
@@ -1,4 +1,5 @@
 const {dbConnection} = require('../config')
+const {UserValidator} = require("../validators")
 class User{
     constructor(userData){
         this.userData=userData
@@ -9,13 +10,21 @@ class User{
             await collection.insertOne(this.userData)
         })
     }
+    static validate(userData){
+        const validationResult = UserValidator.validate(userData)
+        return validationResult
+    }
 }
 
 
 const user = new User({
     name :"aya",
     email:"alharazinaya@gmail.com",
-    username:"ahh1",
-    password:12345
+    username:"aassssssssssssssssssssssssssssssaa",
+    password:"22323"
 })
-user.save()
+const val = user.validate();
+console.log(val);
+
+
+// user.save()
---
+++ 08 - all/validators/user.js
@@ -0,0 +1,9 @@
+const joi = require('@hapi/joi')
+const scheme = joi.object({
+    name : joi.string().required(),
+    email : joi.string().email().required(),
+    username:joi.string().alphanum().min(4).max(10).message('aya must be 10 char').required(),
+    password:joi.string().required()
+})
+
+module.exports = scheme
