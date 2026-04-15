02e82df3eac73841f999cf3cf4960d5ca6550470
ayoosh <alharazinaya@gmail.com>
2026-04-15 20:25:57 +0200

    Add Review model for handling reviews with error management
    - Introduced a new Review model to manage review data and interactions.
    - Implemented a save method that updates or inserts review data into the database with error handling for better feedback on failures.
    - Ensured upsert functionality for seamless review management.

---
+++ 08 - all/models/Review.js
@@ -0,0 +1,59 @@
+const {dbConnection} = require('../config')
+class Review{
+    constructor(reviewData){
+        this.reviewData=reviewData
+    }
+
+
+    async save(){
+         const collection = dbConnection.getCollection('reviews')
+        try{
+         await collection.updateOne(
+            {
+                _book_id:this.reviewData._book_id,
+                _reviewer_id:this.reviewData._reviewer_id
+            },
+            {
+                $set:{
+                    _book_id:this.reviewData._book_id,
+                    _reviewer_id:this.reviewData._reviewer_id,
+                    rating:this.reviewData.rating,
+                    comment:this.reviewData.comment    
+                }
+            },
+            {
+                upsert:true
+            }
+
+         )
+         return{
+            status:true
+         }
+        }catch(err){
+            return{
+                status:false,
+                message:err.message
+            }
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
