4820eca3719d21b2f117ed40f6717ac094f97828
ayoosh <alharazinaya@gmail.com>
2026-04-15 12:00:44 +0200

    Fix formatting in middlewares/index.js by adding a newline at the end of the file for consistency.

---
+++ 08 - all/middlewares/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const {createError} = require('http-error')
+const {readFileSync} = require('fs')
+module.exports=(req,resp,next)=>{
+    const authHeader = req.get('Authorization')
+    if(!authHeader){
+        return next(createError(401,'User not authenticated'))
+    }
+
+    const token = authHeader.split(' ')[1]
+    const secretKey = readFileSync('./config/private.key')
+    try{
+    const decode = jwt.verify(token,secretKey)
+    req._user_id = decode._id
+    req._reviewer_id = decode._reviewer_id
+    next()
+    }catch(err){
+        return next(createError(401))
+    }
+}
